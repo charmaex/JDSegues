d5985e17e7cf01c39ac12b462717314c18b592b4
Jan Dammshäuser <jandammshaeuser@me.com>
2016-08-28 20:05:09 +0200

    Initial Commit

---
+++ JDSegues/JDSegues.h
@@ -0,0 +1,19 @@
+//
+//  JDSegues.h
+//  JDSegues
+//
+//  Created by Jan Dammshäuser on 28.08.16.
+//  Copyright © 2016 Jan Dammshäuser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JDSegues.
+FOUNDATION_EXPORT double JDSeguesVersionNumber;
+
+//! Project version string for JDSegues.
+FOUNDATION_EXPORT const unsigned char JDSeguesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JDSegues/PublicHeader.h>
+
+
