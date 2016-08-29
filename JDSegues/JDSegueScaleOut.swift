227b5f8936f181e9278d5cee8a40f6685d50eb1c
Jan Dammshäuser <jandammshaeuser@me.com>
2016-08-29 14:33:48 +0200

    Added JDSegueScaleOut

---
+++ JDSegues/JDSegueScaleOut.swift
@@ -0,0 +1,58 @@
+//
+//  JDSegueScaleOut.swift
+//  JDSegues
+//
+//  Created by Jan Dammshäuser on 29.08.16.
+//  Copyright © 2016 Jan Dammshäuser. All rights reserved.
+//
+
+import UIKit
+
+/// Segue that scales out to the center of the screen.
+@objc
+public class JDSegueScaleOut: UIStoryboardSegue, JDSegueDelayable, JDSegueOriginable {
+    
+    /// Defines at which point the animation should start
+    /// - parameter Default: center of the screen
+    public var animationOrigin: CGPoint?
+    
+    /// Time the transition animation takes
+    /// - parameter Default: 0.5 seconds
+    public var transitionTime: NSTimeInterval = 0.5
+    
+    /// Time the transition animation is delayed after calling
+    /// - parameter Default: 0 seconds
+    public var transitionDelay: NSTimeInterval = 0
+    
+    /// Animation Curve
+    /// - parameter Default: CurveLinear
+    public var animationOption: UIViewAnimationOptions = .CurveLinear
+    
+    
+    public override func perform() {
+        let sourceVC = self.sourceViewController
+        let destinationVC = self.destinationViewController
+        let destCenter = sourceVC.view.center
+        
+        destinationVC.view.frame = sourceVC.view.frame
+        
+        delay() {
+            sourceVC.view.window!.insertSubview(destinationVC.view, belowSubview: sourceVC.view)
+        
+            UIView.animateWithDuration(self.transitionTime, delay: 0, options: self.animationOption, animations: {
+                
+                if let center = self.animationOrigin {
+                    sourceVC.view.center = center
+                }
+                
+                sourceVC.view.transform = CGAffineTransformMakeScale(0.05, 0.05)
+                
+            }) { finished in
+                
+                sourceVC.presentViewController(destinationVC, animated: false) {
+                    sourceVC.view.transform = CGAffineTransformMakeScale(1, 1)
+                }
+            }
+        }
+    }
+}
